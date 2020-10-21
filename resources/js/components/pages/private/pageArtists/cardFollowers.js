ae3b04ce6ebcfa0934a0eed009a63f7f12ab59b6
Joshua Saubon <joshuasaubon@gmail.com>
2020-10-22 07:47:29 +0800

    a

--- resources/js/components/pages/private/pageArtists/cardFollowers.js
+++ resources/js/components/pages/private/pageArtists/cardFollowers.js
@@ -14,7 +14,42 @@ const CardFollowers = ({ handleSearchFollower, artistInfo }) => {
                 />
                 <br />
                 <br />
-                <div style={{ overflowX: "auto" }}></div>
+                <div style={{ overflowX: "auto" }}>
+                    <Table dataSource={artistInfo.artist.artist_followers}>
+                        <Table.Column
+                            title="Platform"
+                            dataIndex="platform"
+                            key="platform"
+                        />
+                        <Table.Column
+                            title="Name"
+                            dataIndex="display_name"
+                            key="display_name"
+                            render={(text, record) => {
+                                return (
+                                    <a target="_blank" href={record.user_url}>
+                                        {record.display_name}
+                                    </a>
+                                );
+                            }}
+                        />
+                        <Table.Column
+                            title="Email"
+                            dataIndex="email"
+                            key="email"
+                        />
+                        <Table.Column
+                            title="Followed At"
+                            dataIndex="created_at"
+                            key="created_at"
+                            render={(text, record) => {
+                                return moment(record.created_at).format(
+                                    "YYYY-MM-DD hh:mm A"
+                                );
+                            }}
+                        />
+                    </Table>
+                </div>
             </Card>
         </>
     );
