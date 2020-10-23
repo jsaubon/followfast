d200650a0e69eb78467eeb4bd28191cd321b7cfb
Joshua Saubon <joshuasaubon@gmail.com>
2020-10-24 01:38:18 +0800

    fb and google ads scripts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,60 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>FollowFast</title>
-</head>
-<body>
-<div id="app">
 
-</div>
-<script src="{{ asset('js/admin/app.js') }}"></script>
+    <!-- Facebook Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '729150204370054');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" src="https://www.facebook.com/tr?id=729150204370054&ev=PageView
+        &noscript=1" />
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+    <!-- Global site tag (gtag.js) - Google Ads: 808953923 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-808953923"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-808953923');
+    </script>
+</head>
+
+<body>
+    <div id="app">
+
+    </div>
+    <script src="{{ asset('js/admin/app.js') }}"></script>
 </body>
+
 </html>
