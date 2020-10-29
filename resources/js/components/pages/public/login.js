2c4662be408c795d51d6665269c8308e0f27f37a
Joshua Saubon <joshuasaubon@gmail.com>
2020-10-30 03:05:49 +0800

    fixed wicket follow

--- resources/js/components/pages/public/login.js
+++ resources/js/components/pages/public/login.js
@@ -63,7 +63,7 @@ const Login = () => {
                                     marginTop: "20px"
                                 }}
                             >
-                                <Title level={3}>WickedFooll</Title>
+                                <Title level={3}>WickedFollow</Title>
                                 <Text type="secondary">
                                     Sign In to your account
                                 </Text>
